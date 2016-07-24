252afbee8f050107c4acdda1a03ab9493559da2e
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-07-24 14:02:41 -0700

    requests list from server, returns only 10 items of array result

--- app/components/List.js
+++ app/components/List.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var List = require('react-dynamic-list');
 var pageTitle = require('../styles').pageTitle
+var jquery = require('jquery');
 
 var LIST_DATA = {
   items: [{title: 'First title', imageUrl: 'http://goo.gl/WBmFsz'}, {title: 'Second title', imageUrl: 'http://goo.gl/WBmFsz'}, {title: 'Third title', imageUrl: 'http://goo.gl/WBmFsz'}]
@@ -9,32 +10,32 @@ var LIST_DATA = {
 
 var Image = React.createClass({
   render: function () {
-    return <img src='http://facebook.github.io/react/img/logo_og.png'></img>
+    return <img src={this.props.url}></img>
   }
 });
 
 var Content = React.createClass({
   render: function (){
-    return <div>hello</div>
+    return <div>{this.props.content}</div>
   }
 });
 
 var ListItem = React.createClass({
   render: function() {
-    console.log(this);
+
     return <li><div>
-         <h1> {this.props.data.title} </h1>
-         <Image />
-         <Content />
+         <Image url={this.props.data.thumbnailUrl} />
+         <Content content={this.props.data.title} />
        </div></li>;
   }
 });
 
 var ListItems = React.createClass({
   render: function() {
+    var results = this.props.list;
     return (
       <ul>
-        {this.props.list.items.map(function(result) {
+        {results.map(function(result) {
            return <ListItem key={result.id} data={result}/>;
         })}
       </ul>
@@ -43,12 +44,35 @@ var ListItems = React.createClass({
 });
 
 var List = React.createClass({
-  render: function() {
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    jquery.ajax({
+      url: 'http://jsonplaceholder.typicode.com/photos',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data.slice(1, 10)});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('url', status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  render: function () {
     return (
-      <ListItems list={LIST_DATA} />
+      <div>
+         <h1>List</h1>
+         <ListItems list={this.state.data} />
+       </div>
     );
   }
 });
+
+
 
 
 module.exports = List;
