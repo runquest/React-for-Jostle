5173477cbefd3d8a42a01385916026e7841fefb2
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-07-24 08:24:50 -0700

    fading in and out on page transition

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,12 +4,19 @@ var button = require('../styles').button;
 var container = require('../styles').container;
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 var Main = React.createClass({
   render: function () {
     return (
       <div className='main-container' style={container}>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName='appear'
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
         <div style={footer}>
             <button style={button}>
               <Link to='/'>Home</Link>
